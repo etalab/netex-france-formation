7071f7daf8b07d86eee2aa76e1ea5ef70966ac2a
Thibaut Barrère <thibaut.barrere@gmail.com>
2025-03-19 15:22:58 +0100

    Fix snippets references after moving them

--- lib/kino_tooling.ex
+++ lib/kino_tooling.ex
@@ -1,6 +1,8 @@
 defmodule Tooling do
-  def file(path) do
-    (__DIR__ <> "/../" <> path)
+  # TODO: automatically infer `basedir` using `__CALLER__` instead
+  def file(path, basedir) do
+    basedir
+    |> Path.join(path)
     |> File.read!()
   end
 
