fdec10d40e0e21407c1c4f7c61b9afbe17972e45
Thibaut Barrère <thibaut.barrere@gmail.com>
2025-03-20 08:52:46 +0100

    Structuration suite Livebook Rochefort (#2)
    * Create 003-rochefort.livemd
    
    * Update 003-rochefort.livemd
    
    * Ajout image offre
    
    * tests kino
    
    * Complément identification Lignes régulières, scolaires, RPI
    
    * Description de la ligne B
    
    * Route, Direction et RouteLink
    
    * Ajout ScheduledStopPoint, ServiceJourneyPattern, + Partie HORAIRES avec les ServiceJourney
    
    * Ajout TODO list
    
    * Add todos
    
    * Extract XML to files for readability
    
    * Reduce code
    
    * Extract XML for line & reduce code
    
    * Extract XML for third part
    
    * Fin DRY
    
    * Micro-UML + ajout map RouteLink (img)
    
    * Disable git-lfs for subfolder (fixes #5)
    
    * Move livebook for target folder (NOTE: things are broken at this point)
    
    * Fix path
    
    * Create verify.exs
    
    * Move images underneath
    
    * Move snippets underneath
    
    * Fix snippets references after moving them
    
    * Move files to sandbox, fix broken paths
    
    ---------
    
    Co-authored-by: adurand-cs <ariane.durand@cs-soprasteria.com>

---
+++ sandbox-brouillons/scripts/verify.exs
@@ -0,0 +1,10 @@
+# basic squeletton to parse a `.livemd` and detect dead links
+# TODO: verify links
+Mix.install([
+  {:earmark_parser, "~> 1.4"}
+])
+
+file = __DIR__ <> "/../livebooks/1_reseau/reseau_rochefort.livemd"
+
+markdown = File.read!(file)
+{:ok, _ast, []} = EarmarkParser.as_ast(markdown)
