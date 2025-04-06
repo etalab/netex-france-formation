3fa96703b8c326b617e699502eebc0b3ea938ccf
Thibaut Barrère <thibaut.barrere@gmail.com>
2025-04-07 00:24:13 +0200

    Compute summary

--- lib/test/wip_test.exs
+++ lib/test/wip_test.exs
@@ -17,15 +17,45 @@ defmodule Tests do
   @file_path "data/ca_rochefort_ocean-aggregated-netex-2025-03-03.zip"
 
   # TODO: port to xmerl_sax_handler to reduce external dependencies
+  # NOTE: for much larger docs later, this is the way https://github.com/qcam/saxy/pull/100
   defmodule SaxHandler do
     @behaviour Saxy.Handler
 
+    def format_line([item]), do: item
+    def format_line(items = [item | _tail]), do: "#{item} (x#{items |> length})"
+
+    def handle_event(:start_document, _options, []) do
+      {:ok, %{level: 0, output: []}}
+    end
+
+    def handle_event(:end_document, _options, state) do
+      output = state.output
+      |> Enum.chunk_by(& &1)
+      |> Enum.map(&format_line(&1))
+      |> Enum.join("\n")
+
+      {:ok, output}
+    end
+
     def handle_event(:start_element, {name, _attributes}, state) do
-      IO.puts name
+      indent = String.duplicate("  ", state.level)
+
+      state = state
+      |> Map.update!(:level, &(&1 + 1))
+
+      state = state
+      |> Map.update!(:output, fn(output) ->
+        cond do
+          state.level > 5 -> output
+          state.level in (2..3) -> output ++ [indent <> "(...)"]
+          true -> output ++ [indent <> name]
+        end
+      end)
       {:ok, state}
     end
 
     def handle_event(:end_element, _, state) do
+      state = state |> Map.update!(:level, &(&1 - 1))
       {:ok, state}
     end
 
@@ -47,10 +77,11 @@ defmodule Tests do
     |> ZipSupport.list_zip_entries()
     |> one!(~r/commun/)
 
-    zip
+    {:ok, output} = zip
     |> Unzip.file_stream!(filename)
-    |> Stream.into(<<>>, &IO.iodata_to_binary/1)
+    |> Stream.map(&IO.iodata_to_binary(&1))
     |> Saxy.parse_stream(SaxHandler, [])
-    |> IO.puts
+
+    IO.puts output
   end
 end
