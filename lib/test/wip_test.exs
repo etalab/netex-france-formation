b9f3551209de13c010dba0ab5c4390bb44b69902
adurand-cs <ariane.durand@cs-soprasteria.com>
2025-04-07 11:30:38 +0200

    Ameliorations rochefort (#20)
    * Non-distinction scolaire/non-scolaire
    
    * Exercices (part 1)
    
    * Ajout Timestamps (1er jet) + exo course
    
    * Suppression TODO
    
    * Liens autres parties
    
    * Fix broken HTML
    
    * Switch to markdown for collapse
    
    * Save reformatted code
    
    * Save reformatted code
    
    * Move file listing from DataTable to markdown list (try to be clearer)
    
    * Wip test for summariser
    
    * Compute summary
    
    * Format
    
    * Extract XmlSummariser module to file
    
    * Separate dedicated Saxy part before adding more there
    
    * Finalise tests
    
    * Add < >
    
    ---------
    
    Co-authored-by: Thibaut Barrère <thibaut.barrere@gmail.com>

---
+++ lib/test/wip_test.exs
@@ -0,0 +1,56 @@
+# self-contained tests embryo
+
+Mix.install([
+  {:unzip, "~> 0.12.0"},
+  {:saxy, "~> 1.6"}
+])
+
+ExUnit.start()
+Code.require_file("../zip_support.ex", __DIR__)
+Code.require_file("../xml_support.ex", __DIR__)
+Code.require_file("../xml_summariser.ex", __DIR__)
+Code.require_file("../helpers.ex", __DIR__)
+
+defmodule Tests do
+  use ExUnit.Case
+
+  @folder_root Path.join(__DIR__, "../..")
+  @file_path "data/ca_rochefort_ocean-aggregated-netex-2025-03-03.zip"
+
+  def one!(list, regexp) do
+    list
+    |> Enum.filter(&(&1 =~ regexp))
+    |> Helpers.one!()
+  end
+
+  test "summarises XML binary" do
+    content = "<Root><Child/><Child/><Other/><Child/></Root>"
+    summary = XmlSummariser.summarise!(content)
+
+    assert summary == """
+           <Root>
+             (...) (x4)\
+           """
+  end
+
+  test "summarises XML stream" do
+    # useful to ensure we don't break the unzip/iodata/binary dance
+    zip =
+      @file_path
+      |> Path.expand(@folder_root)
+      |> ZipSupport.open_zip!()
+
+    filename =
+      zip
+      |> ZipSupport.list_zip_entries()
+      |> one!(~r/commun/)
+
+    output =
+      zip
+      |> Unzip.file_stream!(filename)
+      |> Stream.map(&IO.iodata_to_binary(&1))
+      |> XmlSummariser.summarise!()
+
+    assert output =~ ~r/\<Line\> \(x14\)/
+  end
+end
